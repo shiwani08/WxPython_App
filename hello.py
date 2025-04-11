111e907ba37221f0def48dffa141067c2169768c
shiwani08 <shiwanisoni29082002@gmail.com>
2025-04-11 11:57:11 +0530

    a simple UI developed

--- hello.py
+++ hello.py
@@ -1,7 +1,58 @@
 import wx
 
-app = wx.App()
+class HelloFrame(wx.Frame):
 
-frm = wx.Frame(None, title = "Hello Duniya!!")
-frm.Show()
-app.MainLoop()
+    def __init__ (self, *args, **kw):
+        super(HelloFrame, self).__init__(*args, **kw)
+
+        pnl = wx.Panel(self)
+
+        st = wx.StaticText(pnl, label="Hellooo Duniya!!")
+        font = st.GetFont()
+        font.PointSize += 10
+        font = font.Bold()
+        st.SetFont(font)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(st, wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25))
+        pnl.SetSizer(sizer)
+
+        self.makeMenuBar()
+
+        self.CreateStatusBar()
+        self.SetStatusText("Welcome to wxpython. Ab ye saanp lega maze!!")
+
+    def makeMenuBar(self):
+        fileMenu = wx.Menu()
+        helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H","Help string shown in the status bar for this menu item")
+        fileMenu.AppendSeparator()
+
+        exitItem = fileMenu.Append(wx.ID_EXIT)
+
+        helpMenu = wx.Menu()
+        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        self.SetMenuBar(menuBar)
+
+        self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
+        self.Bind(wx.EVT_MENU, self.OnExit, exitItem)
+        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+
+    def OnExit(self, event):
+        self.Close(True)
+
+    def OnHello(self, event):
+        wx.MessageBox("Waps aa gya hello bolne main hehe!!")
+
+    def OnAbout(self, event):
+        wx.MessageBox("This is your Captain speaking.")
+
+if __name__ == '__main__':
+    app = wx.App()
+    frm = HelloFrame(None, title='Hello Rupert!')
+    frm.Show()
+    app.MainLoop()
